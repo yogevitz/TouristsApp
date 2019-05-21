808627b194f2ab7a3209efe84015290d8463bf4a
yogevitz <yogevshlomovitz@gmail.com>
2019-05-21 18:31:37 +0300

    Finished API Requests: login, getPOIInfo

--- app.js
+++ app.js
@@ -7,13 +7,49 @@ app.listen(port, function () {
     console.log('Example app listening on port ' + port);
 });
 
-app.get('/select', function(req, res){
-    DButilsAzure.execQuery("SELECT * FROM tableName")
+// getAllUsers
+app.get('/getAllUsers', function(req, res){
+    DButilsAzure.execQuery("SELECT * FROM Users")
         .then(function(result){
             res.send(result)
         })
         .catch(function(err){
-            console.log(err)
+            console.log(err);
             res.send(err)
         })
 });
+
+// getPOIInfo
+app.get('/getPOIInfo/:id', function(req, res) {
+    DButilsAzure.execQuery("SELECT * FROM Points Where id = " + req.params.id)
+        .then(function(result){
+            res.send(result)
+        })
+        .catch(function(err){
+            console.log(err);
+            res.send(err)
+        })
+});
+
+// login
+app.post("/login/:username/:password", (req, res) => {
+    let inputUserName = req.params.username;
+    let inputPassword = req.params.password;
+    let legalUser = false;
+    DButilsAzure.execQuery("SELECT UserName, Password FROM Users")
+        .then(function(result){
+            for (let i = 0; i < result.length; i++) {
+                if (result[i].UserName === inputUserName
+                    && result[i].Password === inputPassword) {
+                    legalUser = true;
+                }
+            }
+            res.send(legalUser);
+        })
+        .catch(function(err){
+            console.log(err);
+            res.send(err)
+        })
+});
+
+
