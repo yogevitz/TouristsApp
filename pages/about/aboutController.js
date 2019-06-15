ec9389a5724211609babba7bd50ce7df2c69dd60
ITAYCARMI <itayccarmi@gmail.com>
2019-06-15 17:58:49 +0300

    about window itay

--- pages/about/aboutController.js
+++ pages/about/aboutController.js
@@ -5,5 +5,19 @@ angular.module("myApp")
     $scope.btnCount = 0;
     $scope.myFunc = function() {
         $scope.btnCount++;
-    }
+    };
+    self = this;
+    self.images = {
+        1: {name:"Rome Symbol", image: "images/symbolRome.jpg"},
+        2: {name:"Rome Flag", image: "images/flagRome.jpg"},
+        3: {name:"Italy Flag", image: "images/flagItaly.jpg"}
+    };
+    self.cityImages = {
+        1: {name:"Colosseum", image: "images/Colosseum.jpg"},
+        2: {name:"Vatican City", image: "images/Vatican.jpg"},
+        3: {name:"Borghese Gallery", image: "images/Borghese.jpg"},
+        4: {name:"Capitoline Museums", image: "images/Capitoline.jpg"},
+        5: {name:"Tonnarello", image: "images/Tonnarello.jpg"},
+        6: {name:"Stadio Olimpico", image: "images/Stadio.jpg"}
+    };
 });
--- pages/register/registerController.js
+++ pages/register/registerController.js
@@ -2,4 +2,19 @@
 angular.module("myApp")
     .controller("registerController", function ($scope) {
         // button click count
+
+        $scope.category = (function () {
+                checked = ($scope.input("input[type=checkbox]:checked")).length;
+                if(!(checked > 1)) {
+                    alert("You must check at least two checkbox.");
+                    return false;
+                }
+                else {
+                    alert("registerd");
+                    return true;
+                }
+
+
+        });
     });
+
