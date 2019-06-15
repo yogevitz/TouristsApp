2e74be5d9a870d58b9b1dd6b056c8ced55c81603
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2019-06-15 19:21:38 +0300

    update register

--- pages/register/registerController.js
+++ pages/register/registerController.js
@@ -1,20 +1,72 @@
-// login controller
+// register controller
 angular.module("myApp")
     .controller("registerController", function ($scope) {
-        // button click count
+        $scope.questions = [
+            "The name of your first dog",
+            "The name of your 3rd grade teacher",
+            "The name of your favorite book",
+            "The name of your favorite restaurant"
+        ];
 
-        $scope.category = (function () {
-                checked = ($scope.input("input[type=checkbox]:checked")).length;
-                if(!(checked > 1)) {
-                    alert("You must check at least two checkbox.");
+        $scope.countries = [
+            "Australia",
+            "Bolivia",
+            "China",
+            "Denemark",
+            "Israel",
+            "Latvia",
+            "Monaco",
+            "August",
+            "Norway",
+            "Panama",
+            "Switzerland",
+            "USA"
+        ];
+
+        $scope.categories = [
+            { id: 1, name: "Food" },
+            { id: 2, name: "Sport" },
+            { id: 3, name: "Museums" },
+            { id: 4, name: "Culture" },
+            { id: 5, name: "Nature" },
+            { id: 6, name: "Art" }
+        ];
+
+        $scope.selectedCategories = [];
+
+        let vm = this;
+        vm.questionsFullList = '';
+        vm.question1List = '';
+        vm.question1List = '';
+
+        $scope.register = function() {
+
+            if (validCategories()) {
+                alert("Valid registration!");
+            } else {
+                return;
+            }
+
+            function validCategories() {
+                if ($scope.selectedCategories.filter(Boolean).length < 2) {
+                    alert("Please choose at least 2 categories");
                     return false;
-                }
-                else {
-                    alert("registerd");
+                } else {
                     return true;
                 }
-
-
-        });
+            }
+        }
     });
+
+angular.module("myApp").filter('excludeUsed', function() {
+    var filter = function(items, excludeVal1, excludeVal2) {
+        var checkItem = function(item) {
+            return (item != excludeVal1) && (item != excludeVal2);
+        };
+
+        return items.filter(checkItem);
+    };
+
+    return filter;
+});
 
